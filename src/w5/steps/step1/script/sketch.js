66f3a60bffbbc7fb314f71d9097ad797d325bbdd
rlatjwls7882 <107532530+rlatjwls7882@users.noreply.github.com>
2023-10-05 17:21:52 +0900

    w5

---
+++ src/w5/steps/step1/script/sketch.js
@@ -0,0 +1,30 @@
+let angle = 0;
+let angularVel = 0;
+let angularAcc;
+const propellerLen = 100;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+  angularAcc = (TAU / 360) * 0.01;
+}
+
+function draw() {
+  background(255);
+
+  angularVel += angularAcc;
+  angularVel = constrain(angularVel, -5, 5);
+  angle += angularVel;
+
+  translate(width / 2, height / 2);
+  rotate(angle);
+
+  stroke(0);
+  strokeWeight(1);
+  fill(127);
+  line(-propellerLen / 2, 0, propellerLen / 2, 0);
+
+  strokeWeight(2);
+  fill(127);
+  circle(propellerLen / 2, 0, 16);
+  circle(-propellerLen / 2, 0, 16);
+}
