7835118605ad03d4fcee9d57ad5a0343d3a661e1
rlatjwls7882 <rlatjwls7882@gmail.com>
2023-10-16 15:18:00 +0900

    w6

---
+++ src/w6/steps/step2/script/sketch.js
@@ -0,0 +1,25 @@
+let particleArray = [];
+let gravity = 0;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+
+  gravity = createVector(0, 0.1);
+
+  background(255);
+}
+
+function draw() {
+  particleArray.push(new Particle(width / 2, 20));
+
+  background(255);
+  for (let a = 0; a < particleArray.length; a++) {
+    particleArray[a].applyForce(gravity);
+    particleArray[a].update();
+    particleArray[a].display();
+  }
+
+  for (let a = particleArray.length - 1; a >= 0; a--) {
+    if (particleArray[a].isDead()) particleArray.splice(a, 1);
+  }
+}
