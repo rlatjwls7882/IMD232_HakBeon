20639cff3086bdeb29a8d7488e35bbfb45154008
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2023-12-03 05:12:16 +0900

    w11

--- src/w11/assignment/step1/script/objSon.js
+++ src/w11/assignment/step1/script/objSon.js
@@ -2,10 +2,4 @@ class ObjSon {
   constructor(x, y) {
     this.pos = createVector(x, y);
   }
-
-  display() {
-    stroke(0);
-    fill(255);
-    ellipse(this.pos.x, this.pos.y, width / 35);
-  }
 }
